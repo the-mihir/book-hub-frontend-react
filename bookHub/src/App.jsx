62ea8598e10ae4f8bcb2a7a403370aa48fd5d3e1
Mihir <mihirkantho@gmail.com>
2024-04-26 18:36:29 +0600

    adding a components

---
+++ bookHub/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from "./Components/Header/Header.jsx";
+
+const App = () => {
+    return (
+        <>
+            <Header/>
+        </>
+    );
+};
+
+export default App;
